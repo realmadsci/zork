9619b1bfc7d74028b53f394946fc7dcb0e179e35
realmadsci <71108352+realmadsci@users.noreply.github.com>
2021-04-01 00:38:14 +0000

    Avoid infinite recursion when exiting
    When exiting the game due to death, the database was closed
    prior to calling exit_() which resulted in failure to be able
    to report the score properly, which resulted in calling exit_().
    
    This checks if the database is closed and skips double-reporting
    the score if the earlier code has already closed it.

--- supp.c
+++ supp.c
@@ -29,10 +29,10 @@ extern time_t time P((time_t *));
 extern struct tm *localtime ();
 
 /* Terminate the game */
-
+extern FILE *dbfile;
 void exit_()
 {
-    score_(0);
+    if (dbfile) score_(0);
     fprintf(stderr, "The game is over.\n");
     exit(0);
 }
