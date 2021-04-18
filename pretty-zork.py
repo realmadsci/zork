707633cb1a1c8d6e12fac850d2b9fbf4219671d4
realmadsci <71108352+realmadsci@users.noreply.github.com>
2021-04-18 21:42:36 +0000

    pretty-zork: Improve sync with game i/o
    * Print the '\n' characters from user input, so we can see when
      there are truly blank lines.
    * Wait for '>' character(s) when reading output, to keep our interaction
      in sync.

--- pretty-zork.py
+++ pretty-zork.py
@@ -14,23 +14,38 @@ input_lines = Path(sys.argv[2]).read_bytes().split(b'\n')
 colorama.init(strip=False) # This will make termcolor work on Windows too!
 
 def showInput(line):
-    cprint(repr(line)[2:-1], 'magenta', attrs=['bold'], end='')
+    cprint(repr(line)[2:-1], 'green', attrs=['bold'], end='')
 
 def showZork(reply):
     for rep_line in reply.decode().split('\n'):
         print()
         print(rep_line, end='')
 
-
+# Start the game:
 zork = process(zork_path)
-reply = zork.recv(timeout=0.1)
-showZork(reply)
-for line in input_lines:
-    showInput(line)
-    zork.sendline(line)
-    reply = zork.recv(timeout=0.1)
-    showZork(reply)
 
-zork.shutdown('send')
-reply = zork.recvall()
+# Wait for the mailbox prompt:
+reply = zork.recvuntil('>', timeout=0.1)
 showZork(reply)
+try:
+    for line in input_lines:
+        showInput(line + b'\n')
+        zork.sendline(line)
+        reply = b''
+        add_reply = b''
+        while True:
+            # NOTE: Need to keep reading '>' until a short timeout, because sometimes multiple commands
+            #       happen in one input line, and Zork will output multiple prompts as it loops through and processes them.
+            add_reply = zork.recvuntil('>', timeout=0.01)
+            reply += add_reply
+            if add_reply == b'':
+                break
+        if reply == b'':
+            reply = zork.recv(timeout=0.01)
+        showZork(reply)
+except EOFError:
+    pass
+finally:
+    zork.shutdown('send')
+    reply = zork.recvall()
+    showZork(reply)
